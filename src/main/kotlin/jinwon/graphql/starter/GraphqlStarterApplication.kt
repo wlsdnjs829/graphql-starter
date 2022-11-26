aa3db611c28db99f319c60d041d93dd24eee4960
ljw0829 <olo_jinwon@naver.com>
2022-11-26 22:34:27 +0900

    - feat : graphql-spring 및 초기 세팅

---
+++ src/main/kotlin/jinwon/graphql/starter/GraphqlStarterApplication.kt
@@ -0,0 +1,11 @@
+package jinwon.graphql.starter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlStarterApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlStarterApplication>(*args)
+}
---
+++ src/test/kotlin/jinwon/graphql/starter/GraphqlStarterApplicationTests.kt
@@ -0,0 +1,13 @@
+package jinwon.graphql.starter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlStarterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
