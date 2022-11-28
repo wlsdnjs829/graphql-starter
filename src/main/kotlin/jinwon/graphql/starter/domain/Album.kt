ddbf03692f1bbba408c2c285a0c389473d9a2df9
ljw0829 <olo_jinwon@naver.com>
2022-11-28 23:45:50 +0900

    - feat : DataFetchers 활용해보기

--- src/main/kotlin/jinwon/graphql/starter/domain/Album.kt
+++
@@ -1,7 +0,0 @@
-package jinwon.graphql.starter.domain
-
-data class Album(
-    val title: String,
-    val artist: String,
-    val recordNo: Int,
-)
--- src/main/kotlin/jinwon/graphql/starter/fetcher/AlbumDataFetcher.kt
+++ src/main/kotlin/jinwon/graphql/starter/fetcher/AlbumDataFetcher.kt
@@ -1,9 +1,14 @@
 package jinwon.graphql.starter.fetcher
 
 import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsData
+import com.netflix.graphql.dgs.DgsDataFetchingEnvironment
 import com.netflix.graphql.dgs.DgsQuery
 import com.netflix.graphql.dgs.InputArgument
-import jinwon.graphql.starter.domain.Album
+import jinwon.graphql.starter.DgsConstants
+import jinwon.graphql.starter.types.Album
+
+private const val SPACE = " "
 
 @DgsComponent
 class AlbumDataFetcher {
@@ -19,8 +24,22 @@ class AlbumDataFetcher {
     @DgsQuery
     fun albums(@InputArgument titleFilter: String?): List<Album>? =
         titleFilter?.run {
-            albums.filter { it.title.contains(this) }
+            albums.filter { it.title?.contains(this) ?: false }
                 .toList()
         } ?: albums
+
+    @DgsData(parentType = DgsConstants.ALBUM.TYPE_NAME)
+    fun artist(dfe: DgsDataFetchingEnvironment): String? {
+        val album = dfe.getSource<Album>()
+        return album.artist
+            ?.split(SPACE)
+            ?.first()
+    }
+
+    @DgsData.List(
+        DgsData(parentType = DgsConstants.ALBUM.TYPE_NAME, field = DgsConstants.ALBUM.RecordNo),
+        DgsData(parentType = DgsConstants.ALBUM.TYPE_NAME, field = DgsConstants.ALBUM.Title)
+    )
+    fun otherData(dfe: DgsDataFetchingEnvironment): Any? = 1
 
 }
