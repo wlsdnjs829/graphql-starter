e85bbc1f9a6486430ed96522e3e98e45b1677c95
ljw0829 <olo_jinwon@naver.com>
2022-11-28 22:44:30 +0900

    - feat : CodeGen 세팅, Generate 돌리기

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.resolve.sam.recreateTypeParametersAndReturnMapping
 
 plugins {
     id("org.springframework.boot") version "2.6.0"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+    id("com.netflix.dgs.codegen") version "5.6.0"
     kotlin("jvm") version "1.7.21"
     kotlin("plugin.jpa") version "1.7.21"
     kotlin("plugin.spring") version "1.7.21"
@@ -19,6 +21,7 @@ repositories {
 
 dependencies {
     implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter:5.4.3")
+    implementation("com.netflix.graphql.dgs.codegen:graphql-dgs-codegen-gradle:5.6.2")
     implementation("com.graphql-java:graphql-java:19.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -28,6 +31,11 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.graphql:spring-graphql-test")
+}
+
+tasks.withType<com.netflix.graphql.dgs.codegen.gradle.GenerateJavaTask> {
+    generateClient = true
+    packageName = "jinwon.graphql.starter"
 }
 
 tasks.withType<KotlinCompile> {
